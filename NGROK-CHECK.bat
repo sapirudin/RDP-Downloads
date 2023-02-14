6e5985bf49dbbb7869ea86036ba750895cc65d1c
chamod12 <65149504+chamod12@users.noreply.github.com>
2023-02-14 10:30:50 +0530

    Add files via upload

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Credits: T.me/Whatforusername
-echo Ip / Host: Check Your Ngrok Account > Cloud Edge > End Point > Click It > Copy Tcp Address
+echo Credits: t.me/Whatforusername
+echo Host: Check Your Ngrok Account > Cloud Edge > End Point > Click It > Copy Tcp Address
 echo Username: runneradmin
 echo Pass: P@ssw0rd!
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://github.com/chamod12/RDP-Downloads/blob/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
